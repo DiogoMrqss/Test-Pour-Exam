2e28215278a140138881586c0fce6a091d06dab4
DiogoMrqss <Diogo.Flrsm@eduge.ch>
2024-06-07 09:12:47 +0200

    jout cOORD

---
+++ Coord.java
@@ -0,0 +1,27 @@
+/************************/
+//NE PAS MODIFIER !!
+/************************/
+
+public class Coord
+{
+        private int x;
+        private int y;
+
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int distanceAvec(Coord autrePosition)
+        {
+            return Math.abs(x-autrePosition.x)+Math.abs(y-autrePosition.y);
+        }
+
+
+        public String toString()
+        {
+            return "("+x+", "+y+")";
+        }
+
+}
